1fabff0660916d67ea8e2f18eb77ebae6ff8a1e5
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-05-27 21:32:30 +0200

    backend implemented

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,7 +1,11 @@
 package com.bookstore;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +14,16 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner init(BookRepository repository) {
+		// Save our starter set of books
+		return args -> {
+			Stream.of(new Book(null, "Horton Hears a Who", "Dr. Seuss"), new Book(null, "A Brief History of Time", "Stephen Hawking"),
+					new Book(null, "Brave New World", "Aldous Huxley")).forEach(book -> {
+				repository.save(book);
+			});
+			//retrieve them all, and print so that we see everything is wired up correctly
+			repository.findAll().forEach(System.out::println);
+		};
+	}
 }
